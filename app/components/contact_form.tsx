674ac528b98650516e84c6bb82e20d6fcc23c4ae
zeshaq <zeshaq@gmail.com>
2024-03-08 11:10:54 +0600

    commit 6

--- app/components/contact_form.tsx
+++ app/components/contact_form.tsx
@@ -7,7 +7,7 @@ const ContactForm = () => {
   return (
    
 
- <div className=''>
+ <div >
 
     <div className='flex flex-row mt-36 gap-32'>
         <div className='basis-5/12'>
@@ -18,7 +18,7 @@ const ContactForm = () => {
          <div className='mt-4'>         
            We are more than happy to have a chat about anything cloud-native.</div>  
 
-         <span>  Give us a call, or leave a note, and we’ll get back to you.</span>  
+         <span>  Give us a call, or leave a note, and we will get back to you.</span>  
            </p>
         </div>
 
@@ -27,14 +27,14 @@ const ContactForm = () => {
             <div className='flex flex-row gap-11'>
 
                 <div className='basis-2/4'>
-                <label for="input-fn" className="block text-sm font-medium mb-2 dark:text-white">First Name</label>
+                <label  className="block text-sm font-medium mb-2 dark:text-white">First Name</label>
                 <input type="text" id="input-fn" 
                 className="py-3 px-4 block w-full border-b-2 border-b-black text-sm
                  focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"   />
                 </div>
 
                 <div className='basis-2/4'>
-                <label for="input-ln" className="block text-sm font-medium mb-2 dark:text-white">Last Name</label>
+                <label  className="block text-sm font-medium mb-2 dark:text-white">Last Name</label>
                 <input type="text" id="input-ln" className="py-3 px-4 block w-full border-b-2 border-b-black text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"  />
                 </div>
             
@@ -47,7 +47,7 @@ const ContactForm = () => {
 
 
                 <div className='basis-full'>
-                <label for="input-cmp" className="block text-sm font-medium mb-2 dark:text-white">Company Information</label>
+                <label  className="block text-sm font-medium mb-2 dark:text-white">Company Information</label>
                 <input type="text" id="input-cmp" className="py-3 px-4 block w-full border-b-2 border-b-black text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"   />
                 </div>
  
@@ -67,12 +67,12 @@ const ContactForm = () => {
             <div className='flex flex-row gap-11 mt-12'>
 
                 <div className='basis-2/4'>
-                <label for="input-email" className="block text-sm font-medium mb-2 dark:text-white">Email</label>
+                <label   className="block text-sm font-medium mb-2 dark:text-white">Email</label>
                 <input type="email" id="input-email" className="py-3 px-4 block w-full border-b-2 border-b-black text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"   />
                 </div>
 
                 <div className='basis-2/4'>
-                <label for="input-label" className="block text-sm font-medium mb-2 dark:text-white">Phone</label>
+                <label className="block text-sm font-medium mb-2 dark:text-white">Phone</label>
                 <input type="phone" id="input-label" className="py-3 px-4 block w-full border-b-2 border-b-black text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"   />
                 </div>
 
@@ -84,7 +84,7 @@ const ContactForm = () => {
 
             <div className='basis-full'>
 
-            <label for="textarea-comm" class="block text-sm font-medium mb-2 dark:text-white">Comment</label>
+            <label className="block text-sm font-medium mb-2 dark:text-white">Comment</label>
             <textarea id="textarea-comm" class="py-3 px-4 border-b-2 border-b-black block w-full border-gray-200  text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" rows="3"  ></textarea>
 
             <button type="button" className="mt-8 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
