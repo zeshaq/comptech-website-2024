cafb0a7ea641929fd76a3bc90d9941c2e974df28
zeshaq <zeshaq@gmail.com>
2024-03-10 02:44:54 +0600

    commit 12

--- app/openstack/page.tsx
+++ app/openstack/page.tsx
@@ -35,11 +35,15 @@ Managed OpenStack Clients
 
             </h1>
             <p className=" text-justify mt-8">
-            Organisations need to work across different enterprise platforms and environments. For that – and in the future – you need robust yet flexible infrastructure.
+            Organisations need to work across different enterprise platforms and environments. 
+            For that – and in the future – you need robust yet flexible infrastructure.
 
-We specialise in RedHat OpenStack solutions – from your core data centres to the network edge. With SIRE, you get our vast experience of working with multiple platforms, plus the transparent and responsible operations you would expect from a trusted IT partner.
+We specialise in Mirantis, RedHat and Canonical OpenStack solutions – from your core data centres to the network edge. 
+With CompTech, you get our vast experience of working with multiple platforms, plus the transparent and 
+responsible operations you would expect from a trusted IT partner.
 
-When you need to make operations more agile, think RedHat OpenStack with SIRE. We don’t just deploy platforms and environments that are ready for the future; we build relationships that last.
+When you need to make operations more agile, think OpenStack with CompTech.
+ We don’t just deploy platforms and environments that are ready for the future; we build relationships that last.
      </p>
         </div>
  
@@ -52,7 +56,10 @@ When you need to make operations more agile, think RedHat OpenStack with SIRE. W
 <h1 className=" text-4xl font-black">End-to-end service</h1>
 
 
-Our pedigree as a provider is always based on infrastructures designed to have zero downtime and run 24/7/365 without a hitch. And our multi-disciplinary in-house team covers every aspect of cloud infrastructure projects – from design to deployment – throughout their lifecycle including:
+Our pedigree as a provider is always based on infrastructures designed to have zero
+ downtime and run 24/7/365 without a hitch. And our multi-disciplinary in-house team 
+ covers every aspect of cloud infrastructure projects – from design to deployment – throughout
+  their lifecycle including:
    </div>
    <div className="grid grid-cols-2 gap-8 p-12">
     <div className="">
@@ -62,14 +69,17 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 
 
 </h1>
-        <p >Our CNCF-certified experts help you simplify the complex landscape of CNCF and navigate between open-source and 
+        <p >Our CNCF-certified experts help you simplify the complex landscape of CNCF and navigate 
+            between open-source and 
             proprietary software choices to find the best solution based on your goals.</p>
     </div>
 
     <div className="">
         <h1 className="2xl font-bold">Data centre deployment engineers</h1>
         <p >
-        Choosing the right technology stack is crucial to your Kubernetes adoption journey. We assist in evaluating and comparing different technologies, engaging with vendors if necessary, and making the right choices
+        Choosing the right technology stack is crucial to your Kubernetes adoption journey.
+         We assist in evaluating and comparing different technologies, engaging with vendors if necessary,
+          and making the right choices
          based on your unique needs and the maturity state of your current platform.
         </p>
     </div>
@@ -81,7 +91,8 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 
         <p >
             
-        We can help you select, implement and configure a service mesh solution like Istio, Linkerd, or Consul. We help you efficiently utilize service meshes to manage traffic,
+        We can help you select, implement and configure a service mesh solution like Istio, Linkerd,
+         or Consul. We help you efficiently utilize service meshes to manage traffic,
          implement robust security policies, and facilitate observability.
             
             </p>
@@ -91,7 +102,8 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 
 </h1>
         <p >
-        Security planning starts at the beginning. From the start, we apply best practices in Kubernetes security to protect your workloads. From secret management 
+        Security planning starts at the beginning. From the start, we apply best practices 
+        in Kubernetes security to protect your workloads. From secret management 
         to network policies, we implement measures to safeguard your environment.
 
         </p>
@@ -102,7 +114,8 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 
 </h1>
         <p >
-        Because security is a continuous concern - not an afterthought - we create secure-by-design infrastructure up front, to minimize vulnerabilities. Our approach includes automating security checks in the deployment process, reinforcing code reviews with security analysis, and ensuring an efficient response to security threats. This proactive, integrated approach minimizes the risk of breaches while maintaining system integrity. The result? Balanced speed, flexibility and risk
+        Because security is a continuous concern - not an afterthought - we create secure-by-design 
+        infrastructure up front, to minimize vulnerabilities. Our approach includes automating security checks in the deployment process, reinforcing code reviews with security analysis, and ensuring an efficient response to security threats. This proactive, integrated approach minimizes the risk of breaches while maintaining system integrity. The result? Balanced speed, flexibility and risk
          management that safeguards your operations without hindering innovation and growth.
 
    </p>
@@ -113,7 +126,8 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 
 </h1>
         <p >
-        Managing your data efficiently within Kubernetes means using appropriate storage solutions. From persistent volumes 
+        Managing your data efficiently within Kubernetes means using appropriate storage solutions. 
+        From persistent volumes 
         to stateful applications, we ensure your data management is optimal and accessible.</p>
     </div>
    </div>
@@ -122,10 +136,13 @@ Our pedigree as a provider is always based on infrastructures designed to have z
 <div className="p-12">
 <h1 className="text-3xl font-black">Every component covered</h1>
 <p className="mt-6 mb-6">
-Our design service encompasses all aspects of the compute, storage, networking and control plane components. In other words, all the essential elements that come together in a fully functioning OpenStack – and we do it from the ground up.
+Our design service encompasses all aspects of the compute, storage, networking and control 
+plane components. In other words, all the essential elements that come together in a fully 
+functioning OpenStack – and we do it from the ground up.
 </p>
 
-<h1 className="text-6xl font-black mt-12">Beyond the physical build and the installation, we make sure your business is up and running with:</h1>
+<h1 className="text-6xl font-black mt-12">Beyond the physical build and the installation, we make sure your 
+business is up and running with:</h1>
 <div className=" grid grid-cols-3 gap-12 mt-12">
     <div>
    <h1 className="text-xl font-bold"> 
@@ -159,7 +176,7 @@ Our design service encompasses all aspects of the compute, storage, networking a
 
     <div>
    <h1 className="text-xl font-bold">
-   Monitoring operations of your platform from our UK-based support centre
+   Monitoring operations of your platform from our support centre
 </h1> 
     </div>
 
@@ -182,9 +199,12 @@ Our design service encompasses all aspects of the compute, storage, networking a
 <div className="p-12">
 <h1 className=" text-3xl font-black">Right tool for the job</h1>
 <p>
-Whether your deployment happens in a public, private or hybrid environment, RedHat OpenStack platform can meet – and exceed – your SLAs in uptime, stability, performance, security and cost efficiency.
+Whether your deployment happens in a public, private or hybrid environment,
+ OpenStack platform can meet – and exceed – your SLAs in uptime, stability, performance, security and cost efficiency.
 </p>
-Just as important as it is to have the right tools available to you, the orchestration equally matters. For running of all types of cloud infrastructure, our RedHat OpenStack deployment team will work with you to deliver best-practice
+Just as important as it is to have the right tools available to you, the orchestration equally matters. 
+For running of all types of cloud infrastructure, our OpenStack deployment team will work with you
+ to deliver best-practice
  solutions that are tailored to your needs and the specifics of your environment.
 </div>
 
@@ -196,9 +216,13 @@ Just as important as it is to have the right tools available to you, the orchest
 
 <h1 className=" text-2xl font-bold">  Sound architecture by design</h1> 
 <p className="mt-6">
-Experience tells us that the success of any OpenStack deployment is underpinned by careful analysis of the intended purpose. This is the optimum way to ensure a sound architecture design and cost-effective hardware inventory.
+Experience tells us that the success of any OpenStack deployment is underpinned by careful analysis of
+ the intended purpose. This is the optimum way to ensure a sound architecture design and cost-effective 
+ hardware inventory.
 
-Our design service encompasses all aspects of the compute, storage, networking and control plane components.We leave no component out of the picture when it comes to constructing a fully functioning OpenStack deployment – from the ground up.
+Our design service encompasses all aspects of the compute, storage, networking and control
+ plane components.We leave no component out of the picture when it comes to constructing a fully 
+ functioning OpenStack deployment – from the ground up.
 </p>
     </div>
 
@@ -208,11 +232,17 @@ Our design service encompasses all aspects of the compute, storage, networking a
     <h1 className=" text-2xl font-bold">    Built by multi-disciplinary teams
     </h1>
 
-    <p className="mt-6">Our skilled RedHat OpenStack team tailor support to your needs. So, we’ll provide a detailed equipment bill of materials, plus cable-by-cable physical build instructions and diagrams. If you prefer, our team of data centre engineers to rack, stack and perform the complete physical build in readiness for the configuration and OpenStack software layers.
+    <p className="mt-6">Our skilled RedHat OpenStack team tailor support to your needs. So, we’ll provide 
+    a detailed equipment bill of materials, plus cable-by-cable physical build instructions and diagrams.
+     If you prefer, our team of data centre engineers to rack, stack and perform the complete physical build in
+      readiness for the configuration and OpenStack software layers.
 
-Following physical build, our software and networking teams will perform all aspects of the OpenStack software deployment. These include configuration and meshing of the environment into your public and/or private network infrastructures.
+Following physical build, our software and networking teams will perform all aspects of the OpenStack 
+software deployment. These include configuration and meshing of the environment into your public and/or
+ private network infrastructures.
 
-Post-build, our service continues with resilience and failover handling tests, plusload testing and production readiness assessments.
+Post-build, our service continues with resilience and failover handling tests, plusload 
+testing and production readiness assessments.
 
 </p>
 
