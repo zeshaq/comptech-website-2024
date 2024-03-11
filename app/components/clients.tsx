e1a3b1cd726f7f8794d01ac8dec92d8460d03a31
zeshaq <zeshaq@gmail.com>
2024-03-11 16:06:57 +0600

    spellings fixed

--- app/components/clients.tsx
+++ app/components/clients.tsx
@@ -10,11 +10,11 @@ const Clients = () => {
   <div className=" text-center mb-6 mt-24">
     <h2 className="text-5xl font-black  p-20 ">
         
-        Trusted by the Government Organizations, Development Sector, Defense and  Leading Institutions in Bangladesh</h2>
+        Trusted by the Government Organizations, Development Sector, Defense and  Leading Financial Institutions in Bangladesh</h2>
   </div>
 
  
-<div className='grid grid-cols-6 pt-12 p-32  gap-24'>
+<div className='grid grid-cols-6 pt-12 p-32  gap-12'>
  
 
     <div className='relative  h-40 '>
